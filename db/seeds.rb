10e0aa1b72e7d837746164e6163f89257c0242f4
BryceMCA <brycemitchell608@gmail.com>
2016-05-10 15:56:56 -0500

    Seed Database with intial data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+message = Messages.create([{author: 'Bryce'}, {content: 'This is a message'])
